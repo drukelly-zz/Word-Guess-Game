b35534c3ddd71608b609306e1be185781b3c27a1
Dru Kelly <drukelly@gmail.com>
2019-03-10 06:43:49 -0700

    starting point (bare)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Word Guess Game
+
+A Hangman-style game
---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// TODO
+// - determine words that will contain an array
+// - research game mechanics
+// - swap _ to guessed letter when guessed correctly
+// - play sound when entire word is complete
+// - start a new round
+
+const words = [];
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
