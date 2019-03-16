cbfd5bbf0b2d7cd54c0ce938dac6b3e5091d1c7d
Dru Kelly <drukelly@gmail.com>
2019-03-15 23:41:27 -0700

    modal remaining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,4 @@
 // TODO
-// - hp
-// - start a new round
-// - background sound
 // - modal
 
 // Arrays and Variables
@@ -15,7 +12,7 @@ let sfCharacters = [
     "sound" : "shoryuken"
   },
   {
-    "name"  : "e. honda",
+    "name"  : "e.honda",
     "sound" : "flying-headbutt"
   },
   {
@@ -51,7 +48,7 @@ let sfCharacters = [
     "sound" : "tiger-uppercut"
   },
   {
-    "name"  : "m. bison",
+    "name"  : "m.bison",
     "sound" : ""
   },
   {
@@ -59,14 +56,7 @@ let sfCharacters = [
     "sound" : ""
   }
 ];
-let guesses = [];
-let user = {
-  "chances" : 0,
-  "score" : 0
-}
-let cpu = {
-  "score" : 0
-}
+let wrongGuesses = [];
 // Selectors
 const profilePhoto = document.getElementsByClassName("profile-photo")[0];
 const gameboard = document.getElementsByClassName("gameboard")[0];
@@ -78,7 +68,7 @@ const displayModal = (message) => {
   console.log(message);
 }
 const isAlpha = (letter) => {
-  if (letter.match(/^[A-Za-z]+$/) || letter === " " || letter === "." || letter === "-") {
+  if (letter.match(/^[A-Za-z]+$/) || letter === "." || letter === "-") {
     return true;
   }
 }
@@ -86,7 +76,7 @@ const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 }
 const nextRound = () => {
-
+  window.location.reload(false);
 }
 const playSound = (file, state) => {
   let audioPlayer = new Audio();
@@ -109,46 +99,41 @@ document.addEventListener("DOMContentLoaded", () => {
   let selectedCharacter = sfCharacters[getRandomInt(sfCharacters.length)];
   let arr = [...selectedCharacter["name"]];
   arr.forEach(char => {
-    gameboard.innerHTML += `<span class="letter letter-inactive" data-letter="${char}">_</span>`;
+    gameboard.innerHTML += `<span class="letter" data-letter="${char}">_</span>`;
   });
-  let targetLetters = document.querySelectorAll("span[data-letter]");
-  let perHitDamage = Math.round(100 / arr.length);
-  user["chances"] = arr.length;
-  console.log(`per hit => ${perHitDamage}%,`, "chances => " + user["chances"]);
+  const targetLetters = document.querySelectorAll("span[data-letter]");
+  const perHitDamage = Math.round(100 / arr.length);
+  console.log(`per hit => ${perHitDamage}%`);
   // onkeypress
   document.onkeydown = (event) => {
     let guess = event.key;
-    if (isAlpha(guess) && arr.indexOf(guess[0]) !== -1) {
+    if (isAlpha(guess) && selectedCharacter["name"].indexOf(guess) !== -1) {
       targetLetters.forEach(targetLetter => {
         targetLetterAttrs = targetLetter.attributes;
         if (guess === targetLetterAttrs[1].value) {
           targetLetter.textContent = guess;
-          targetLetter.classList.remove("letter-inactive");
-          cpuHP.style.width = `${arr.length * perHitDamage}%`
+          targetLetter.classList.add("letter-active");
+          let activeLetters = document.querySelectorAll(".letter-active");
+          cpuHP.style.width = `${[...activeLetters].length * perHitDamage}%`;
+          if (activeLetters.length === selectedCharacter["name"].length) {
+            renderImage(selectedCharacter["name"]);
+            (!selectedCharacter.sound) ? playNoSound() : playSound(selectedCharacter.sound, "play");
+            displayModal("you win!");
+            cpuHP.style.width = "100%";
+            window.setTimeout(() => nextRound(), 2000);
+          }
         }
       });
     } else {
       // losing logic
-      --user["chances"];
-      guesses.push(event.key);
-      console.log(guesses.length * perHitDamage);
-      // guesses.push(guess);
-      playerHP.style.width = `${perHitDamage}%`;
-      if (user["score"] === 10) {
-        displayModal("you win");
-        nextRound();
+      wrongGuesses.push(guess);
+      playerHP.style.width = `${wrongGuesses.length * perHitDamage}%`;
+      if (wrongGuesses.length === selectedCharacter["name"].length) {
+        playSound("lose", "play");
+        displayModal("YOU LOSE");
+        playerHP.style.width = "100%";
+        window.setTimeout(() => nextRound(), 2000);
       }
-      // runs out of guesses
-      if (user["chances"] === 0) {
-        displayModal("you lose");
-        nextRound();
-      }
-    }
-    // for debugging purposes
-    if (event.key === "Enter") {
-      console.log(arr);
-      (!selectedCharacter.sound) ? playNoSound() : playSound(selectedCharacter.sound, "play");
-      renderImage(selectedCharacter.name);
     }
   }
 });
